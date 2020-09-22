8d6338a8a34d97538dbfc1374851e0281715e63a
Edinaldo Júnior <edinaldojuniorqf@gmail.com>
2020-09-22 13:50:12 -0300

    Configurado docker

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,14 +1,12 @@
 module.exports = {
   type: 'mongodb',
-  host: 'localhost',
+  host: 'db',
   port: '27017',
-  // username: '',
-  password: '123',
   database: 'customer-debts',
   useUnifiedTopology: true,
   entities: [
     process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
       ? './src/modules/**/infra/typeorm/schemas/*.ts'
-      : 'dist/modules/**/infra/**/typeorm/schemas/*.js',
+      : 'dist/modules/**/infra/typeorm/schemas/*.js',
   ],
 };
